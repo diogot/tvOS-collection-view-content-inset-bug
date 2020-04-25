fcc7012f102c600a6b68e0e4298b8d4fd2f4c4b1
Diogo Tridapalli <diogot@users.noreply.github.com>
2020-04-25 20:51:01 -0300

    Initial Commit

---
+++ TvOS collection view content inset bug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TvOS collection view content inset bug
+//
+//  Created by Diogo on 25/04/20.
+//  Copyright © 2020 Diogo Tridapalli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
